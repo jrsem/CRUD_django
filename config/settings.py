3ae569d7ca5f674bc886158f9454c49fffcdd7d1
jrsem <juniorsemerzier@gmail.com>
2022-05-27 23:16:49 +0200

    first commit

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,12 +10,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$wj%ipg#l5c+v@u!#)vs@ml2x2#b1$u%isa58o54bg^z^q+37&'
+# SECRET_KEY = 'django-insecure-$wj%ipg#l5c+v@u!#)vs@ml2x2#b1$u%isa58o54bg^z^q+37&'
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', '-d#y@*ob)s0y*g0xa1yu=zn-+=305$bjshn%&4y(ggyne57me=q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pilcode-crud.herokuapp.com']
 
 
 # Application definition
@@ -32,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,12 +73,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# HEROKU
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {'default': dj_database_url.config(conn_max_age=500)}
+else:
+    # local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
